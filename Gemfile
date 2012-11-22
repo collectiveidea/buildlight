29db036d487d4aab5fd7c36cb5520e7872c41491
Daniel Morrison <daniel@collectiveidea.com>
2012-11-22 13:58:33 -0500

    Generated new rails-api app
    Also remove unused files

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'rails-api'
+
+gem 'sqlite3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buildlight::Application.config.secret_token = 'f47bcb0ac1b272776f6ffd5272839d038eb8ec6fd5c4d1607c65fe6b4bc523db2746539460d0a0de80af49ee86d2fc85d4ba91ce8011c767127d82e538858246'
