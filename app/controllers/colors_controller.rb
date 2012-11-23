cc20b490096571877399f520c1f37be564a7cd2e
Daniel Morrison <daniel@collectiveidea.com>
2012-11-23 14:15:50 -0500

    Refactored colors

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,18 +1,5 @@
 class ColorsController < ApplicationController
   def index
-    yellow = green = false
-    status = Status.order('created_at DESC').first
-    if status.color == 'yellow'
-      yellow = true
-      previous = Status.order('created_at DESC').where("color != 'yellow'").first
-    end
-
-    if status.color == 'green' || (previous && previous.color == 'green')
-      green = true
-    end
-
-    render :json => {red: !green,
-                     yellow: yellow,
-                     green: green }
+    render :json => Status.colors
   end
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,8 @@
 class Status < ActiveRecord::Base
   attr_accessible :project, :status
 
+  scope :recent, order('created_at DESC')
+
   # Set color based on travis-ci's status code
   def status_code=(code)
     self.color = case code
@@ -11,5 +13,18 @@ class Status < ActiveRecord::Base
       else
         'yellow'
       end
+  end
+
+  def self.colors
+    yellow = green = false
+    color = recent.first.try(:color)
+    if color == 'yellow'
+      yellow = true
+      green = recent.where("color != 'yellow'").first.try(:color) == 'green'
+    elsif color == 'green'
+      green = true
+    end
+    
+    {red: !green, yellow: yellow, green: green }
   end
 end
