6e26294a36fceda40f9a91743b4c75e879fc8d21
Daniel Morrison <daniel@collectiveidea.com>
2012-11-23 12:27:18 -0500

    Turn on other stuff

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,9 +2,9 @@ class WebhooksController < ApplicationController
   def create
     json = JSON.parse params[:payload]
     @status = Status.new
-    # @status.project_id   = json[:repository][:id]
-    # @status.project_name = json[:repository][:name]
-    # @status.status       = json[:status_message]
+    @status.project_id   = json["repository"]["id"]
+    @status.project_name = json["repository"]["name"]
+    @status.status       = json["status_message"]
     @status.payload = request.body.read
     @status.save!
     head :ok
