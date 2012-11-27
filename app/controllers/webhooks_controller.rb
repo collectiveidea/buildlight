1b68acbdb429c45c2fe0e78e3fd7edc6f5cecc97
Daniel Morrison <daniel@collectiveidea.com>
2012-11-27 10:50:20 -0500

    Add a new webhook for inspection purposes

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class WebhooksController < ApplicationController
   def create
     json = JSON.parse params[:payload]
@@ -8,5 +9,16 @@ class WebhooksController < ApplicationController
     Rails.logger.warn "AUTH: #{@status.username}/#{@status.project_name} with: #{request.headers['Authorization']}"
     @status.save!
     head :ok
+  end
+
+  private
+  def authorize(status, authentication)
+    string = "#{status.username}/#{status.project_name}#{User.find_or_create_by_username(status.username).travis_token}"
+    if Digest::SHA256.new.hexdigest(string) == authentication
+      true
+    else
+      Rails.logger.warn "AUTH FAILURE: #{status.username}/#{status.project_name} with: #{authentication}"
+      true
+    end
   end
 end
