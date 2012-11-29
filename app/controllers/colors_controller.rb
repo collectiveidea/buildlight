11a22f84aeb5bed8cbc95f701af63878fb4d016f
Daniel Morrison <daniel@collectiveidea.com>
2012-11-29 12:22:10 -0500

    Use respond_to

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -2,19 +2,20 @@ class ColorsController < ApplicationController
   include ActionController::Live
 
   def index
-    if params[:format] == 'ryg'
-      begin
-        response.headers['Content-Type'] = 'text/ryg'
-        loop do
-          response.stream.write Status.uncached { Status.ryg(@ids) }
-          sleep 1
+    respond_to do |format|
+      format.ryg do
+        begin
+          response.headers['Content-Type'] = 'text/ryg'
+          loop do
+            response.stream.write Status.uncached { Status.ryg(@ids) }
+            sleep 1
+          end
+        rescue IOError
+          response.stream.close
         end
-      rescue IOError
-        response.stream.close
       end
-    else
-      render json: Status.colors(@ids)
-    end    
+      format.json { render json: Status.colors(@ids) }
+    end
   end
 
   def show
