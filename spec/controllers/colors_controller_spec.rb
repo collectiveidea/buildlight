91a83c3f1bb571da404b6c50f3f38a7fa6f438c5
Daniel Morrison <daniel@collectiveidea.com>
2013-06-18 07:24:33 -0400

    Fix tests since we don't default to JSON anymore

--- spec/controllers/colors_controller_spec.rb
+++ spec/controllers/colors_controller_spec.rb
@@ -4,7 +4,7 @@ describe ColorsController do
   describe "index" do
     it "shows the red light on if the last status is red" do
       FactoryGirl.create :status, red: true
-      get :index
+      get :index, :format => :json
       json = JSON.parse(response.body)
       expect(json['red']).to be_true
     end
@@ -12,7 +12,7 @@ describe ColorsController do
     it "shows the red and yellow lights on if the last status is yellow, but previous non-yellow was red" do
       FactoryGirl.create :status, red: true
       2.times { FactoryGirl.create :status, red: false, yellow: true }
-      get :index
+      get :index, :format => :json
       json = JSON.parse(response.body)
       expect(json['yellow']).to be_true
       expect(json['red']).to be_true
@@ -21,7 +21,7 @@ describe ColorsController do
     it "shows the red light on if the last status is green, but another project is red" do
       FactoryGirl.create :status, red: true
       FactoryGirl.create :status, red: false
-      get :index
+      get :index, :format => :json
       json = JSON.parse(response.body)
       expect(json['red']).to be_true
     end
@@ -31,7 +31,7 @@ describe ColorsController do
     it "shows the status for a single user" do
       FactoryGirl.create :status, username: 'collectiveidea', red: false
       FactoryGirl.create :status, username: 'danielmorrison', red: true
-      get :show, id: 'collectiveidea'
+      get :show, id: 'collectiveidea', :format => :json
       json = JSON.parse(response.body)
       expect(json['red']).to be_false
     end
@@ -39,7 +39,7 @@ describe ColorsController do
     it "shows the status for all users separated by a comma" do
       FactoryGirl.create :status, username: 'collectiveidea', red: false, yellow: true
       FactoryGirl.create :status, username: 'danielmorrison', red: true,  yellow: false
-      get :show, id: 'collectiveidea,danielmorrison'
+      get :show, id: 'collectiveidea,danielmorrison', :format => :json
       json = JSON.parse(response.body)
       expect(json['red']).to be_true
       expect(json['yellow']).to be_true
