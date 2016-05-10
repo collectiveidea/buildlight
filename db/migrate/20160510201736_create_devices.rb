a9fbc7acd7f4015ebf63a1cd38d980a0661807b5
Daniel Morrison <daniel@collectiveidea.com>
2016-05-10 16:25:07 -0400

    Add Device model

---
+++ db/migrate/20160510201736_create_devices.rb
@@ -0,0 +1,12 @@
+class CreateDevices < ActiveRecord::Migration
+  def change
+    enable_extension 'uuid-ossp'
+
+    create_table :devices, id: :uuid do |t|
+      t.string :usernames, array: true, default: [], null: false
+      t.string :projects, array: true, default: [], null: false
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20121124190606) do
+ActiveRecord::Schema.define(version: 20160510201736) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "devices", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
+    t.string   "usernames",  default: [], null: false, array: true
+    t.string   "projects",   default: [], null: false, array: true
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
-    t.string   "project_id",   limit: 255
-    t.string   "project_name", limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "project_id"
+    t.string   "project_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "payload"
     t.boolean  "red"
     t.boolean  "yellow"
-    t.string   "username",     limit: 255
+    t.string   "username"
   end
 
-  add_index "statuses", ["project_id"], name: "index_statuses_on_project_id"
-  add_index "statuses", ["project_name"], name: "index_statuses_on_project_name"
-  add_index "statuses", ["red"], name: "index_statuses_on_red"
-  add_index "statuses", ["username", "project_name"], name: "index_statuses_on_username_and_project_name"
-  add_index "statuses", ["username", "red"], name: "index_statuses_on_username_and_red"
-  add_index "statuses", ["username", "yellow"], name: "index_statuses_on_username_and_yellow"
-  add_index "statuses", ["username"], name: "index_statuses_on_username"
-  add_index "statuses", ["yellow"], name: "index_statuses_on_yellow"
+  add_index "statuses", ["project_id"], name: "index_statuses_on_project_id", using: :btree
+  add_index "statuses", ["project_name"], name: "index_statuses_on_project_name", using: :btree
+  add_index "statuses", ["red"], name: "index_statuses_on_red", using: :btree
+  add_index "statuses", ["username", "project_name"], name: "index_statuses_on_username_and_project_name", using: :btree
+  add_index "statuses", ["username", "red"], name: "index_statuses_on_username_and_red", using: :btree
+  add_index "statuses", ["username", "yellow"], name: "index_statuses_on_username_and_yellow", using: :btree
+  add_index "statuses", ["username"], name: "index_statuses_on_username", using: :btree
+  add_index "statuses", ["yellow"], name: "index_statuses_on_yellow", using: :btree
 
 end
