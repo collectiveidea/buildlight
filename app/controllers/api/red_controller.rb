ecfe18b4eb3360f2a556fc1677300a70066f6272
Daniel Morrison <daniel@collectiveidea.com>
2022-01-27 10:29:20 -0500

    Convert find_by! to find_sole_by

--- app/controllers/api/red_controller.rb
+++ app/controllers/api/red_controller.rb
@@ -1,6 +1,6 @@
 class API::RedController < ApplicationController
   def show
-    device = Device.find_by!(identifier: params[:id])
+    device = Device.find_sole_by(identifier: params[:id])
     @red_projects = device.statuses.where(red: true)
     respond_to do |format|
       format.html
