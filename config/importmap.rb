04716e875087a75dca91b7979279f84dc66f01c8
Daniel Morrison <daniel@collectiveidea.com>
2022-01-28 12:53:51 -0500

    Whitespace fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
-
 pin "application", preload: true
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
